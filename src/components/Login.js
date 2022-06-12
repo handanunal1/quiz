5b59093a882bc80390b5e132fc721479f2671dc9
handan unal <handan.unal@yahoo.com>
2022-06-12 16:40:44 +0300

    validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,87 @@
-import { Button, TextField } from '@mui/material'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Button, Card, CardContent, TextField, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import Center from './Center'
+import useForm from '../hooks/useForm'
+
+const getFreshModel = () => ({
+    name: '',
+    email: ''
+})
 
 export default function Login() {
-    return (
 
-        <form noValidate autoComplete="off">
-            <TextField label="Email" name="Email" variant="outlined"></TextField>
-            <TextField label="Name" name="Name" variant="outlined"></TextField>
-            <Button type='submit' variant='contained' size='large'>Start</Button>
-        </form>
+
+
+    const {
+        values,
+        setValues,
+        errors,
+        setErrors,
+        handleInputChange
+    } = useForm(getFreshModel);
+
+    useEffect(() => {
+      
+    }, [])
+
+
+    const login = e => {
+        e.preventDefault();
+        if (validate())
+ {
+
+ }
+           
+    }
+
+    const validate = () => {
+        let temp = {}
+        temp.email = (/\S+@\S+\.\S+/).test(values.email) ? "" : "Email is not valid."
+        temp.name = values.name != "" ? "" : "This field is required."
+        setErrors(temp)
+        return Object.values(temp).every(x => x == "")
+    }
+
+    return (
+        <Center>
+            <Card sx={{ width: 400 }}>
+                <CardContent sx={{ textAlign: 'center' }}>
+                    <Typography variant="h3" sx={{ my: 3 }}>
+                        Quiz App
+                    </Typography>
+                    <Box sx={{
+                        '& .MuiTextField-root': {
+                            m: 1,
+                            width: '90%'
+                        }
+                    }}>
+                        <form noValidate autoComplete="off" onSubmit={login}>
+                            <TextField
+                                label="Email"
+                                name="email"
+                                value={values.email}
+                                onChange={handleInputChange}
+                                variant="outlined"
+                                {...(errors.email && { error: true, helperText: errors.email })} />
+                            <TextField
+                                label="Name"
+                                name="name"
+                                value={values.name}
+                                onChange={handleInputChange}
+                                variant="outlined"
+                                {...(errors.name && { error: true, helperText: errors.name })} />
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                size="large"
+                                sx={{ width: '90%' }}>Start</Button>
+                        </form>
+                    </Box>
+                </CardContent>
+            </Card>
+        </Center>
+
+
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CssBaseline } from '@mui/material/';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  typography:{
+    fontFamily:'"IBM Plex Sans"'
+  }
+})
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<React.StrictMode>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
